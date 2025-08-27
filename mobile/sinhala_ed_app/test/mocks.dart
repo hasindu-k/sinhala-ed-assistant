e0a140b3b77554cfe6d763ee3194c34ccbc18892
Hasindu Koshitha <koshithahasindu@gmail.com>
2025-08-27 19:21:37 +0530

    add register test

---
+++ mobile/sinhala_ed_app/test/mocks.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:sinhala_ed_app/data/services/firestore_service.dart';
+import 'package:sinhala_ed_app/features/auth/controller/auth_controller.dart';
+
+@GenerateMocks([AuthController, FirestoreService])
+void main() {}
