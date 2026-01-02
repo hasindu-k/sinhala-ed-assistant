c280d9464dbff5209cbc00283d85fa1ec2691669
Miyuri15 <miyurilokuhewage15@gmail.com>
2026-01-02 14:37:16 +0530

    fix - add cors origin

--- app/main.py
+++ app/main.py
@@ -24,6 +24,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:64050",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
     ],
